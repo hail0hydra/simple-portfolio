6d7da27960d17f4401a95a8ffc2408dc514e47eb
hail0hydra <107790736+hail0hydra@users.noreply.github.com>
2025-09-06 10:10:01 +0530

    mod: index.htm

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    base: '/simple-portfolio/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
 })
 
